8386b9d8411d873c4c5ab5ca96dc65912b4075d2
Vitaliy Tokarev <vitaliy.tokarev@elecard.ru>
2019-01-15 15:14:22 +0700

    Change sctp send timeout calculation
    Signed-off-by: Vitaliy Tokarev <vitaliy.tokarev@elecard.ru>

--- src/SCTPWrapper.cpp
+++ src/SCTPWrapper.cpp
@@ -498,7 +498,7 @@ void SCTPWrapper::GSForSCTP(ChunkPtr chunk, uint16_t sid, uint32_t ppid) {
   while (tries < 3000 && shouldSend) {
       if (usrsctp_sendv(this->sock, chunk->Data(), chunk->Length(), NULL, 0, &spa, sizeof(spa), SCTP_SENDV_SPA, 0) < 0) {
           //logger->error("FAILED to send, trying again in {} ms. Retry count: {}", tries, tries);
-          std::this_thread::sleep_for(std::chrono::milliseconds(tries));
+          std::this_thread::sleep_for(std::chrono::milliseconds(1));
           tries += 1;
       } else {
           return;
